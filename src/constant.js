ab15960a02e1b0373d9439c8a5c3bffd81f48f7b
omshewale <omshewale030@gmail.com>
2024-09-21 00:52:42 -0400

    dialog changes

--- src/constant.js
+++ src/constant.js
@@ -2,8 +2,7 @@ export const scale_factor = 4;
 
 export const dialogueData = {
     pc: `This is my macbook. I work mostly in on AI/ML related projects these days.
-    I've made a couple of cool projects with Deep learning. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/omshewale30" target="_blank">Github</a>!`,
+    I've made a couple of cool projects with Deep learning. Anyway regardless of the language, I just like programming.`,
     "cs-degree": `This is my BS in CS degree from ASU. I hung it on the wall because I'm proud of it as I got 4.0 GPA through all 8 semesters. It was an amazing journey, I made a lot of friends and learned a lot of things. I'm grateful for the experience.`,
     "sofa-table": `That's my couch. I like to relax here and have some brewskis with my buddies.`,
     tv: `That's my TV. Big football guy. Bang bang, niner gang. I've been watching a lot of startup stories recently like :
@@ -11,10 +10,7 @@ export const dialogueData = {
     bed: `Although, as a CS major, I do not get much sleep but I try my best . I have been into reading business books recently as I want to start my owm startup someday`,
     resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1CJeyb-z7u7G7NUdb2CB8Gfy43KO0DG2L/view?usp=sharing" target="_blank">Check it out?</a>
   Contact me at omshewale030@gmail.com if you have any interesting job opportunities!`,
-    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
-    library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+    projects: `I have a diverse array of projects under my belt as I like to try new frameworks and languages. Here is my <a href="https://github.com/omshewale30" target="_blank">Github</a>`,
+    library: `There are a lot of self-improvement books on my shelves. One of my heroes is David Goggins. His book "Can't Hurt Me" is a must-read for anyone who wants to push their limits.That is the first book I read from end to end.`,
     exit: `If you want to exit Om's portfolio, just close the tab.`,
 };
